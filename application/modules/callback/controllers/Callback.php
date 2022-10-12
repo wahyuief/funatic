b1aaf30bd6c3c36097ec1b5fe2c019457843bbd9
Wahyu <wahyuief@gmail.com>
2022-10-12 17:16:04 +0700

    fix

--- application/modules/callback/controllers/Callback.php
+++ application/modules/callback/controllers/Callback.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Payment extends FrontendController {
+class Callback extends FrontendController {
 
 	public function __construct()
     {
